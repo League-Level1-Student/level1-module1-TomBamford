a89f783d26e1db35bb148e78bd02a0f50ef42d79
TomBamford <66886044+TomBamford@users.noreply.github.com>
2021-02-27 14:29:04 -0800

    Week 1 of Level 1 Module 1

--- src/_05_vault/vault.java
+++ src/_05_vault/vault.java
@@ -2,8 +2,6 @@ package _05_vault;
 
 public class vault {
 	int secretCode = 0;
-	public tryCode(vault v) {
 	
-	}
 	// tests all of the numbers from 0 - 1 million, accepts the parameter number, if the for loop i matches the number 
 }
