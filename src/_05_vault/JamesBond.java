aeb69ae6adac08ce30eaee36e97c4615488e16db
TomBamford <66886044+TomBamford@users.noreply.github.com>
2021-03-06 14:26:26 -0800

    Week 2 of Level 1

--- src/_05_vault/JamesBond.java
+++ src/_05_vault/JamesBond.java
@@ -1,5 +1,18 @@
 package _05_vault;
 
 public class JamesBond {
-
+	public static void main(String[] args) {
+		vault v = new vault(21320);
+		public findCode(vault v) {
+			for(int i = 0; i < 1000000; i++) {
+				if (cracked = true) {
+					return i;
+				}
+				else if (cracked = false) {
+					return -1;
+				}
+			}
+		}
+	}
 }
+
--- src/_05_vault/vault.java
+++ src/_05_vault/vault.java
@@ -1,7 +1,29 @@
 package _05_vault;
 
+import javax.swing.JOptionPane;
+
 public class vault {
-	int secretCode = 0;
 	
-	// tests all of the numbers from 0 - 1 million, accepts the parameter number, if the for loop i matches the number 
+	public boolean cracked = false;
+	private int secretCode;
+	
+	
+		public vault(int secretCode) {
+			String code = JOptionPane.showInputDialog("Choose a number from zero to one million as your vault code");
+			secretCode = Integer.parseInt(code);
+					public int tryCode(int secretCode) {
+						if (i = secretCode) {
+							
+							
+						}
+					}
+				this.secretCode = secretCode;
 }
+}				
+	
+	
+
+	
+	
+	// tests all of the numbers from 0 - 1 million, accepts the parameter number, if the for loop i matches the number, then return true else return false
+
